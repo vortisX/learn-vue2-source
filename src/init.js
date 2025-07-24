3a5254c80cfe0c48702dcd14c5a8766b37087335
vortisX <vortisX@hotmail.com>
2025-07-24 23:55:30 +0800

    feat: 初始化数据后获取el html内容

--- src/init.js
+++ src/init.js
@@ -1,17 +1,31 @@
 import { initState } from "./initState";
 
 export function initMixin(Vue) {
-    // 在Vue构造函数上添加_init方法
-    Vue.prototype._init = function(option) {
-        console.log("初始化方法被调用,入参:", option);
-        // 将传入的配置数据保存到实例上
-        // 这里可以添加更多的初始化逻辑
-        let vm = this;
-        console.log("Vue 实例:", vm);
-        // 将配置数据保存到实例上
-        vm.$options = option;
-        // 初始化状态
-        initState(vm);
-        console.log("初始化完成");
-    };
+  // 在Vue构造函数上添加_init方法
+  Vue.prototype._init = function (option) {
+    console.log("初始化方法被调用,入参:", option);
+    // 将传入的配置数据保存到实例上
+    // 这里可以添加更多的初始化逻辑
+    let vm = this;
+    console.log("Vue 实例:", vm);
+    // 将配置数据保存到实例上
+    vm.$options = option;
+    // 初始化状态
+    initState(vm);
+    // 渲染模板 el
+    if (vm.$options.el) {
+      vm.$mount(vm.$options.el);
+    }
+  };
+  Vue.prototype.$mount = function (el) {
+    let vm = this;
+    if (!vm.$options.render) {
+      // 如果用户没有传入render函数，则使用模板编译
+      const template = vm.$options.template;
+      if (!template && el) {
+        // 如果没有模板且有el，则获取el的innerHTML作为模板
+        vm.$options.template = document.querySelector(el).innerHTML;
+      }
+    }
+  };
 }
