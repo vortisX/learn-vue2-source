73c4830db07d58ce6f15b76877ce55876b40cc0a
vortisX <vortisX@hotmail.com>
2025-07-28 23:54:02 +0800

    feat: 更新index.html，添加样式和数据绑定；完善AST生成逻辑，新增generate.js文件

---
+++ src/compile/generate.js
@@ -0,0 +1,81 @@
+/**I
+ *  <div id="app">
+ *    <span>hello</span>
+ *   <span>world</span>
+ * </div>
+ *
+ * render() { _c('div', {id: 'app'},
+ *  [_c('span', {}, ['hello']),
+ * _c('span', {}, ['world'])])
+ *  }
+ */
+
+import { defaultTagRE } from "./rule";
+
+function genProps(attrs) {
+  let str = "";
+  for (let i = 0; i < attrs.length; i++) {
+    let attr = attrs[i];
+    if (attr.name === "style") {
+      let obj = {};
+      attr.value.split(";").forEach((element) => {
+        let [key, value] = element.split(":");
+        obj[key.trim()] = value.trim();
+      });
+      attr.value = obj;
+      str += `${attr.name}:${JSON.stringify(attr.value)},`;
+    }
+  }
+  return `{${str.slice(0, -1)}}`;
+}
+
+function genChildren(ast) {
+  let children = ast.children;
+  if (children) {
+    return children.map((item) => gen(item)).join(",");
+  }
+}
+
+function gen(node) {
+  // 1.文本
+  // 2.标签
+  if (node.type === "text") {
+    // 1. 普通文本 2. 含有{{}}
+    let text = node.text;
+    if (!defaultTagRE.test(text)) {
+      return `_v(${JSON.stringify(text)})`;
+    }
+    let tokens = [];
+    let lastIndex = (defaultTagRE.lastIndex = 0);
+    let match;
+    while ((match = defaultTagRE.exec(text))) {
+      console.log(match, "match");
+      let index = match.index;
+      // 添加内容
+      if (index > lastIndex) {
+        tokens.push(JSON.stringify(text.slice(lastIndex, index)));
+      }
+      tokens.push(`_s(${match[1].trim()})`);
+      lastIndex = index + match[0].length;
+    }
+    if (lastIndex < text.length) {
+      tokens.push(JSON.stringify(text.slice(lastIndex)));
+    }
+    return `_v(${tokens.join("+")})`;
+  } else if (node.type === "element") {
+    return generate(node);
+  }
+}
+
+/**
+ * 将AST转换为代码字符串
+ * @param {*} ast
+ */
+export function generate(ast) {
+  let children = genChildren(ast);
+  let code = `_c(${ast.tag},${
+    ast.attrs.length ? `${genProps(ast.attrs)}` : "null"
+  },${children ? `${children}` : "null"})`;
+  console.log(code);
+  return code;
+}
--- src/compile/index.js
+++ src/compile/index.js
@@ -1,5 +1,9 @@
+import { generate } from "./generate.js";
 import { parseHtml } from "./parseAst.js";
 export function compileToFunction(template) {
+  // 将html 变为 ast语法树
   let ast = parseHtml(template);
-  console.log(ast, "ast");
+
+  // ast语法树变为render函数（1） ast语法树变为字符串 （2）字符串变为函数
+   let code = generate(ast);
 }
